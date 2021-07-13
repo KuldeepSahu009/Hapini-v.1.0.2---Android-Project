6da20763e9aca9356a9df7d7973ad535ab8a66a5
Asus <shuklasuyash52@gmail.com>
2021-07-13 20:02:55 +0530

    suyash shukla 12/07/2021 19:33 Spinner created with forms and details

--- app/src/main/java/com/crm/pvt/hapinicrm/Admin/Onlyformasterlogin.java
+++ app/src/main/java/com/crm/pvt/hapinicrm/Admin/Onlyformasterlogin.java
@@ -5,17 +5,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.crm.pvt.hapinicrm.R;
 
 public class Onlyformasterlogin extends AppCompatActivity {
 TextView formastersignup;
+Button mstrlogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onlyformasterlogin);
         formastersignup=findViewById(R.id.TOWARDSMASTERSIGNUP);
+        mstrlogin=findViewById(R.id.btnMasterLogin);
+        mstrlogin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(Onlyformasterlogin.this,MasterAdmin.class);
+                startActivity(intent);
+            }
+        });
         formastersignup.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
