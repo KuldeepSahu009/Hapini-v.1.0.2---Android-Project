4078d21e977cfa36b71385ee8cbc41f186e789d7
satyam <satyam98358@gmail.com>
2021-07-23 17:01:34 +0530

    project

--- app/src/main/java/com/crm/pvt/hapinicrm/ui/MasterDashboardFragment.java
+++ app/src/main/java/com/crm/pvt/hapinicrm/ui/MasterDashboardFragment.java
@@ -15,5 +15,7 @@ public class MasterDashboardFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_master_dashboard, container, false);
+
+
     }
 }
