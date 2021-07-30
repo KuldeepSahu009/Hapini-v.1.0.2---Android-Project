2391c1574f6a4739c7f6514b6320ac7c2d7ae47c
satyam <satyam98358@gmail.com>
2021-07-30 16:18:26 +0530

    projects

--- app/src/main/java/com/crm/pvt/hapinicrm/ui/StartFragment.java
+++ app/src/main/java/com/crm/pvt/hapinicrm/ui/StartFragment.java
@@ -1,5 +1,8 @@
 package com.crm.pvt.hapinicrm.ui;
 
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -7,19 +10,33 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.crm.pvt.hapinicrm.databinding.FragmentStartBinding;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
 
 public class StartFragment extends Fragment {
+
 
     private FragmentStartBinding binding;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+
+
+
+
         binding = FragmentStartBinding.inflate(inflater,container,false);
         return binding.getRoot();
     }
@@ -35,4 +52,5 @@ public class StartFragment extends Fragment {
                 Navigation.findNavController(v).navigate(StartFragmentDirections.actionStartFragmentToUserLoginFragment())
         );
     }
+
 }
