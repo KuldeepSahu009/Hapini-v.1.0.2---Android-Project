bb5d26fd9b552cd5e408f91dd4c11d56011db9fc
ParamSaini <74251396+param-18@users.noreply.github.com>
2021-07-31 18:54:28 +0530

    Data Admin can send task to any data user

--- app/src/main/java/com/crm/pvt/hapinicrm/ui/Addtask.java
+++ app/src/main/java/com/crm/pvt/hapinicrm/ui/Addtask.java
@@ -1,5 +1,7 @@
 package com.crm.pvt.hapinicrm.ui;
 
+import android.app.ProgressDialog;
+import android.icu.text.StringSearch;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -14,57 +16,117 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.crm.pvt.hapinicrm.R;
+import com.crm.pvt.hapinicrm.databinding.FragmentAddtaskBinding;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.Objects;
 
 
 public class Addtask extends Fragment {
-    Button sendtask;
-    EditText passcode,task;
-    ImageView back;
 
-
+    private EditText passcode , taskReceived;
+    private Button sendtask;
+    private static String state;
+    private FragmentAddtaskBinding binding;
+    private FirebaseDatabase dataBaseInstance;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-       View view= inflater.inflate(R.layout.fragment_addtask, container, false);
-       passcode=view.findViewById(R.id.enterpasscodeforaddtask);
-       sendtask=view.findViewById(R.id.sendtask);
-       task=view.findViewById(R.id.addtasksettask);
-        back=view.findViewById(R.id.dataentryaddtaskbackimg);
+       binding = FragmentAddtaskBinding.inflate(inflater, container, false);
+       dataBaseInstance = FirebaseDatabase.getInstance();
+       passcode=binding.enterpasscodeforaddtask;
+       sendtask=binding.sendtask;
+       taskReceived=binding.addtasksettask;
+        binding.dataentryaddtaskbackimg.setOnClickListener(v -> Navigation.findNavController(v).navigateUp());
+        state = sendtask.getText().toString();
 
-
-       sendtask.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               String passcodes=passcode.getText().toString();
-               String tasks=task.getText().toString();
-               if(passcodes.length()<6){
-                   passcode.setError("Check passcode");
-               }else if(TextUtils.isEmpty(tasks)){
-                   task.setError("Cannot sent empty");
-               }
-               else{
-                settask(passcodes,tasks);
-               }
+       sendtask.setOnClickListener(v -> {
+           if(state.equals("VERIFY"))
+           {
+               verifyUser();
            }
-       });
+           else
+           {
 
-       back.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View v) {
-               Navigation.findNavController(v).navigateUp();
            }
        });
 
 
-       return view;
-    }
-    private void settask(String passcodes,String tasks){
-       passcode.setText("");
-       task.setText("");
+
+       return binding.getRoot();
     }
 
+    private void verifyUser() {
+        String passcodes=passcode.getText().toString();
+        if(passcodes.length()<6){
+            passcode.setError("Check passcode");
+        }
+        else {
+            ProgressDialog progressDialog = new ProgressDialog(getContext());
+            progressDialog.setCancelable(false);
+            progressDialog.setTitle("Verifying User....");
+            progressDialog.show();
+            dataBaseInstance.getReference().child("usersv2").child("data").equalTo(passcodes).get().addOnSuccessListener(task -> {
+                if (task != null) {
+                    passcode.setVisibility(View.GONE);
+                    taskReceived.setVisibility(View.VISIBLE);
+                    sendtask.setText("SEND TASK");
+                    progressDialog.dismiss();
+                    sendTaskToUser(task);
+                } else {
+                    Toast.makeText(getContext(), "No Such User Exist.", Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+    }
+   private FirebaseAuth auth;
+    private void sendTaskToUser(DataSnapshot task) {
+        auth = FirebaseAuth.getInstance();
+        String tasks=taskReceived.getText().toString();
+        if(TextUtils.isEmpty(tasks)){
+            taskReceived.setError("Cannot sent empty");
+        }
+        else{
+            ProgressDialog progressDialog = new ProgressDialog(getContext());
+            progressDialog.setCancelable(false);
+            progressDialog.setTitle("Sending Task To User....");
+            progressDialog.show();
+            task.getRef().child(passcode.getText().toString()).child("task").setValue(tasks).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task1) {
+                    if(task1.isSuccessful())
+                    {
+                       task.getRef().child(passcode.getText().toString()).child("taskGivenBy").setValue(auth.getCurrentUser().getEmail()).addOnCompleteListener(new OnCompleteListener<Void>() {
+                           @Override
+                           public void onComplete(@NonNull Task<Void> task) {
+                               if(task.isSuccessful())
+                               {
+                                   progressDialog.dismiss();
+                                   Toast.makeText(getContext(),"Task given to that user" ,Toast.LENGTH_SHORT).show();
+                                   Navigation.findNavController(getView()).navigateUp();
+                               }
+                               else
+                               {
+                                   Toast.makeText(getContext(),"Something going Wrong" ,Toast.LENGTH_LONG).show();
+                               }
+                           }
+                       });
+                    }
+                    else
+                    {
+                        Toast.makeText(getContext(),"Something Went Wrong" ,Toast.LENGTH_LONG).show();
+                    }
+                }
+            });
+        }
+    }
 
 }
