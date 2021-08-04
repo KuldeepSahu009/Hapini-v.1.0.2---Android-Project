5d0151db0897fe6f949fc89f429a8ad1e4c4fa93
Aditya Singh <deviladi00@gmail.com>
2021-08-04 16:04:53 +0530

    Updated Admin Delete 04 August 21

--- app/src/main/java/com/crm/pvt/hapinicrm/adapters/TrackAdminAdapter.java
+++ app/src/main/java/com/crm/pvt/hapinicrm/adapters/TrackAdminAdapter.java
@@ -1,32 +1,36 @@
 package com.crm.pvt.hapinicrm.adapters;
 
+import static com.crm.pvt.hapinicrm.ui.AdminDataViewFragment.type;
+
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
 import com.crm.pvt.hapinicrm.R;
 import com.crm.pvt.hapinicrm.model.Admin;
+import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
+import java.util.Objects;
 
 public class TrackAdminAdapter extends RecyclerView.Adapter<TrackAdminAdapter.TrackAdminViewHolder> {
 
     Context context;
-    List<Admin> admins;
+    ArrayList<Admin> admins;
 
     public TrackAdminAdapter( Context context){
         this.context = context;
@@ -50,10 +54,54 @@ public class TrackAdminAdapter extends RecyclerView.Adapter<TrackAdminAdapter.Tr
         holder.password.setText(admin.getPassword());
         holder.location.setText(admin.getLocation());
 
+        holder.deleteAdmin.setOnClickListener(v -> {
+
+            DatabaseReference reference = FirebaseDatabase.getInstance().getReference("adminV2").child(type);
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
+            builder.setTitle("Attention");
+            builder.setMessage("Do you want to delete this admin?");
+            builder.setCancelable(true);
+
+            builder.setPositiveButton("Yes",(dialog, which) -> reference.addValueEventListener(new ValueEventListener() {
+
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+
+                        if(admin.getPasscode().equals(Objects.requireNonNull(dataSnapshot.getValue(Admin.class)).getPasscode())){
+
+                            Log.i("TrackAdminAdapter","Admin Found");
+                            reference.child(Objects.requireNonNull(dataSnapshot.getKey())).removeValue().addOnSuccessListener(unused -> {
+                                admins.remove(admin);
+                                setAdmins(admins);
+                                Snackbar.make(v,"Admin removed ",Snackbar.LENGTH_SHORT).show();
+                            });
+
+                        }
+
+                    }
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) { }
+            }));
+            builder.setNegativeButton("No",(dialog, which) -> { });
+
+            AlertDialog deleteAdminDialog = builder.create();
+            deleteAdminDialog.show();
+        });
+
         if (!admin.getImgurl().isEmpty()){
             Glide.with(context).load(admin.getImgurl()).into(holder.profilepic);
         }
 
+    }
+
+    @Override
+    public void setHasStableIds(boolean hasStableIds) {
+        super.setHasStableIds(hasStableIds);
     }
 
     @Override
@@ -64,7 +112,17 @@ public class TrackAdminAdapter extends RecyclerView.Adapter<TrackAdminAdapter.Tr
         return admins.size();
     }
 
-    public void setAdmins(List<Admin> admins) {
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return position;
+    }
+
+    public void setAdmins(ArrayList<Admin> admins) {
         this.admins = admins;
         notifyDataSetChanged();
     }
