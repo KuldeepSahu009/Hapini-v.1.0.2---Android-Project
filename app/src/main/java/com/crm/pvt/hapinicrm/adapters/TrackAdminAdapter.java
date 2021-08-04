6f67782eea23bfa90969ff20b3b322957c3266bc
satyam <satyam98358@gmail.com>
2021-08-04 19:01:23 +0530

    projects

--- app/src/main/java/com/crm/pvt/hapinicrm/adapters/TrackAdminAdapter.java
+++ app/src/main/java/com/crm/pvt/hapinicrm/adapters/TrackAdminAdapter.java
@@ -3,6 +3,7 @@ package com.crm.pvt.hapinicrm.adapters;
 import static com.crm.pvt.hapinicrm.ui.AdminDataViewFragment.type;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,6 +18,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.bumptech.glide.Glide;
 import com.crm.pvt.hapinicrm.R;
 import com.crm.pvt.hapinicrm.model.Admin;
+import com.crm.pvt.hapinicrm.ui.Datacallbacktrackuser;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -31,9 +33,14 @@ public class TrackAdminAdapter extends RecyclerView.Adapter<TrackAdminAdapter.Tr
 
     Context context;
     ArrayList<Admin> admins;
+    Datacallbacktrackuser datacallbacktrackuser;
+    private static final String TAG = "TAG";
+    public static String usertyepes;
 
-    public TrackAdminAdapter( Context context){
+    public TrackAdminAdapter( Context context,ArrayList<Admin> admins,Datacallbacktrackuser datacallbacktrackuser){
         this.context = context;
+        this.admins=admins;
+        this.datacallbacktrackuser=datacallbacktrackuser;
     }
 
     @NonNull
@@ -56,38 +63,30 @@ public class TrackAdminAdapter extends RecyclerView.Adapter<TrackAdminAdapter.Tr
 
         holder.deleteAdmin.setOnClickListener(v -> {
 
-            DatabaseReference reference = FirebaseDatabase.getInstance().getReference("adminV2").child(type);
+
 
             AlertDialog.Builder builder = new AlertDialog.Builder(v.getContext());
             builder.setTitle("Attention");
             builder.setMessage("Do you want to delete this admin?");
             builder.setCancelable(true);
 
-            builder.setPositiveButton("Yes",(dialog, which) -> reference.addValueEventListener(new ValueEventListener() {
-
+            builder.setPositiveButton("ok", new DialogInterface.OnClickListener() {
                 @Override
-                public void onDataChange(@NonNull DataSnapshot snapshot) {
-
-                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
-
-                        if(admin.getPasscode().equals(Objects.requireNonNull(dataSnapshot.getValue(Admin.class)).getPasscode())){
-
-                            Log.i("TrackAdminAdapter","Admin Found");
-                            reference.child(Objects.requireNonNull(dataSnapshot.getKey())).removeValue().addOnSuccessListener(unused -> {
-                                admins.remove(admin);
-                                setAdmins(admins);
-                                Snackbar.make(v,"Admin removed ",Snackbar.LENGTH_SHORT).show();
-                            });
-
-                        }
-
-                    }
+                public void onClick(DialogInterface dialog, int which) {
+                    if (admins.size()>0){
+                    datacallbacktrackuser.remove(admins.get(position),usertyepes);}
+                    dialog.dismiss();
                 }
-
+            });
+            builder.setNegativeButton("cancel", new DialogInterface.OnClickListener() {
                 @Override
-                public void onCancelled(@NonNull DatabaseError error) { }
-            }));
-            builder.setNegativeButton("No",(dialog, which) -> { });
+                public void onClick(DialogInterface dialog, int which) {
+                    Log.e(TAG, "onClick: "+"cancel" );
+                    dialog.dismiss();
+                }
+            });
+
+
 
             AlertDialog deleteAdminDialog = builder.create();
             deleteAdminDialog.show();
@@ -99,33 +98,15 @@ public class TrackAdminAdapter extends RecyclerView.Adapter<TrackAdminAdapter.Tr
 
     }
 
-    @Override
-    public void setHasStableIds(boolean hasStableIds) {
-        super.setHasStableIds(hasStableIds);
-    }
+
 
     @Override
     public int getItemCount() {
-        if(admins == null) {
-            return 0;
-        }
+
         return admins.size();
     }
 
-    @Override
-    public long getItemId(int position) {
-        return position;
-    }
 
-    @Override
-    public int getItemViewType(int position) {
-        return position;
-    }
-
-    public void setAdmins(ArrayList<Admin> admins) {
-        this.admins = admins;
-        notifyDataSetChanged();
-    }
 
     static class TrackAdminViewHolder extends RecyclerView.ViewHolder{
         ImageView profilepic,deleteAdmin;
