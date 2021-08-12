f4f43e983ef1807b912ce633bcb3f41c389cb238
Asus <shuklasuyash52@gmail.com>
2021-08-12 12:51:54 +0530

    Latest

--- app/src/main/java/com/crm/pvt/hapinicrm/forgetpassword_user.java
+++ app/src/main/java/com/crm/pvt/hapinicrm/forgetpassword_user.java
@@ -1,9 +1,14 @@
 package com.crm.pvt.hapinicrm;
 
+import static android.content.ContentValues.TAG;
+import static android.widget.Toast.LENGTH_LONG;
 import static android.widget.Toast.LENGTH_SHORT;
+
+import static com.crm.pvt.hapinicrm.ui.UserLoginFragment.isUserLoggedIn;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -17,6 +22,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.crm.pvt.hapinicrm.R;
@@ -58,12 +64,19 @@ Button submituser;
             String email=getpasswordemailuser.getText().toString();
             String passcode=passcodepassworduser.getText().toString();
             String Usertype=usertype.getText().toString();
-            if(!email.equals("")&&(passcode.length()==6)) {
+            if(!email.equals("")&&(passcode.length()==6)&&(Usertype.contains("crm")||
+                    Usertype.contains("data")||Usertype.contains("video"))) {
                 senddetails(email, passcode, Usertype);
-                Toast.makeText(getContext(),"Details Submitted",LENGTH_SHORT).show();
+
+
             }
             else{
-                Toast.makeText(getContext(),"Not Valid Details",LENGTH_SHORT).show();
+               Toast toast= Toast.makeText(getContext(),"Not Valid Details",LENGTH_SHORT);
+                View views=toast.getView();
+                views.setBackgroundColor(Color.WHITE);
+                TextView toastmessage=(TextView) toast.getView().findViewById(android.R.id.message);
+                toastmessage.setTextColor(Color.BLACK);
+                toast.show();
             }
         });
 
@@ -77,11 +90,39 @@ Button submituser;
         myRef.child("passcodes").setValue(passcode);
         myRef.child("UserType").setValue(usertype);
         myRef.child("reason").setValue("Request for password");
-     getdatafromfirebase();
+     getdatafromfirebase(passcode,usertype);
     }
 
-    private void getdatafromfirebase() {
-        //I am working
+    private void getdatafromfirebase(String passcodes, String usertype) {
+        DatabaseReference myref2 = FirebaseDatabase.getInstance().getReference("usersv2").child(usertype);
+        // Read from the database
+        myref2.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                    String key = dataSnapshot.getKey();
 
+                    if (key.equals(passcodes)) {
+                        String getpassword = dataSnapshot.child("password").getValue().toString();
+                        Toast toast= Toast.makeText(getContext(),"Password is"+getpassword,LENGTH_LONG);
+                        View views=toast.getView();
+                        views.setBackgroundColor(Color.WHITE);
+                        TextView toastmessage=(TextView) toast.getView().findViewById(android.R.id.message);
+                        toastmessage.setTextColor(Color.BLACK);
+
+                        toast.show();
+                    }
+                    else{
+                        Toast.makeText(getContext(),"No Such Detail Available",LENGTH_LONG).show();
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
     }
 }
