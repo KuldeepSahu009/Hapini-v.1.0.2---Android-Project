e3f8695679edf559d706ceae8eb2316556cd21f5
Asus <shuklasuyash52@gmail.com>
2021-08-13 21:04:13 +0530

    Latest

--- app/src/main/java/com/crm/pvt/hapinicrm/ui/FranchiseDashboardFragment.java
+++ app/src/main/java/com/crm/pvt/hapinicrm/ui/FranchiseDashboardFragment.java
@@ -10,16 +10,18 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
+import com.crm.pvt.hapinicrm.R;
 import com.crm.pvt.hapinicrm.databinding.FragmentFranchiseDashboardBinding;
 
 public class FranchiseDashboardFragment extends Fragment {
 
     private FragmentFranchiseDashboardBinding binding;
-
+    public static String addAdminTypes;
+    private Bundle admin;
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         binding = FragmentFranchiseDashboardBinding.inflate(inflater,container,false);
+        admin = new Bundle();
         return binding.getRoot();
     }
 
@@ -31,6 +33,15 @@ public class FranchiseDashboardFragment extends Fragment {
                     .findNavController(v)
                     .navigate(FranchiseDashboardFragmentDirections
                             .actionFranchiseDashboardFragmentToFranchiseUserChatFragment());
+        });
+        binding.adminaddfromfranchise.setOnClickListener(v -> {
+            addAdminTypes = "CRM";
+            Navigation.findNavController(v).navigate(FranchiseDashboardFragmentDirections.
+                    actionFranchiseDashboardFragmentToAddAdminFormDetailsFragment2());
+        });
+        binding.trackadminaddfromfranchise.setOnClickListener(v -> {
+            admin.putString("ADMIN" , "crm");
+            Navigation.findNavController(v).navigate(R.id.action_franchiseDashboardFragment_to_adminDataViewFragment2,admin);
         });
     }
 }
